fc96f6f51b510894be01f18e6c57d53cf0d19405
chengbin <cheng_bin122@163.com>
2020-11-23 16:46:24 +0800

    add filterWithRest

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 export function add(a, b) {
   return a + b;
 }
+export const filterWithRest = (data, handler) => {
+  const satisfied = [];
+  const rest = [];
+
+  for (let i = 0, len = data.length; i < len; i++) {
+    const current = data[i];
+
+    handler(current) ? satisfied.push(current) : rest.push(current);
+  }
+
+  return [satisfied, rest];
+};
